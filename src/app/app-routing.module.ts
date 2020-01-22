11dc4d0c164345de40c80d5cbefdf42f0aad7f50
Malama <charlesmalama89@gmail.com>
2020-01-22 20:00:03 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OpenTicketComponent } from './pages/open-ticket/open-ticket.component';
+import { TicketListComponent } from './pages/ticket-list/ticket-list.component';
+import { PendingTicketsComponent } from './pages/pending-tickets/pending-tickets.component';
+import { ClosedTicketsComponent } from './pages/closed-tickets/closed-tickets.component';
+import { OverDueTicketsComponent } from './pages/over-due-tickets/over-due-tickets.component';
+import { AssignedTicketsComponent } from './pages/assigned-tickets/assigned-tickets.componet';
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/openTicket' },
+  { path: 'openTicket', pathMatch: 'full', component: OpenTicketComponent},
+  { path: 'ticketList', pathMatch: 'full', component: TicketListComponent},
+  { path: 'assignedTickets', pathMatch: 'full', component: AssignedTicketsComponent},
+  { path: 'viewOpenTickets', pathMatch: 'full', component: PendingTicketsComponent },
+  { path: 'viewAClosedTickets', pathMatch: 'full', component: ClosedTicketsComponent},
+  { path: 'viewOverDueTickets', pathMatch: 'full', component: OverDueTicketsComponent}
+  
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { OpenTicketComponent } from './pages/open-ticket/open-ticket.component';
+import { TicketListComponent } from './pages/ticket-list/ticket-list.component';
+import { ClosedTicketsComponent } from './pages/closed-tickets/closed-tickets.component';
+import { OverDueTicketsComponent } from './pages/over-due-tickets/over-due-tickets.component';
+import { PendingTicketsComponent } from './pages/pending-tickets/pending-tickets.component';
+import { AssignedTicketsComponent } from './pages/assigned-tickets/assigned-tickets.componet';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OpenTicketComponent,
+    TicketListComponent,
+    AssignedTicketsComponent,
+    ClosedTicketsComponent,
+    OverDueTicketsComponent,
+    PendingTicketsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
